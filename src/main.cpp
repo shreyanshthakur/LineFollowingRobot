aab02b2fa7cd0404546d5ba48dcb54a8864d5e60
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2026-06-25 13:04:28 +0530

    feat/hello-world code

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  delay(1000);
+  Serial.println("Hello World!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  Serial.println("Hello from ESP32");
+  delay(1000);
 }
