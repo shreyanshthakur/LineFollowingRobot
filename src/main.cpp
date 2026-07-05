4ffc83ff93c22132a7a5d97a3dd32889ec63a7fd
Shreyansh Singh Thakur <thakur.shreyansh11@gmail.com>
2026-07-05 17:58:33 +0530

    functionality/Line-following-code

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,65 @@
-#include <Arduino.h>
+#include<Arduino.h>
 
 #define LEFT_SENSOR_PIN 18
 #define RIGHT_SENSOR_PIN 19
 
+#define IN1 32
+#define IN2 33
+#define IN3 25
+#define IN4 26
+
+void moveForward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void turnLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void turnRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void stopMoving() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
 void setup() {
-    Serial.begin(115200);
-    pinMode(LEFT_SENSOR_PIN, INPUT);
-    pinMode(RIGHT_SENSOR_PIN, INPUT);
+  Serial.begin(115200);
+  pinMode(LEFT_SENSOR_PIN, INPUT);
+  pinMode(RIGHT_SENSOR_PIN, INPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
 }
 
 void loop() {
-    int leftValue = digitalRead(LEFT_SENSOR_PIN);
-    int rightValue = digitalRead(RIGHT_SENSOR_PIN);
+  int leftValue = digitalRead(LEFT_SENSOR_PIN);
+  int rightValue = digitalRead(RIGHT_SENSOR_PIN);
 
-    Serial.print("L: ");
-    Serial.print(leftValue);
-    Serial.print(" | R: ");
-    Serial.println(rightValue);
+  if (leftValue == LOW && rightValue == LOW) {
+    moveForward();
+  } else if (leftValue == LOW) {
+    turnLeft();
+  } else if (rightValue == LOW) {
+    turnRight();
+  } else {
+    stopMoving();
+  }
 
-    if (leftValue == LOW && rightValue == LOW) {
-        Serial.println("Both on line");
-    } else if (leftValue == LOW) {
-        Serial.println("Left on line");
-    } else if (rightValue == LOW) {
-        Serial.println("Right on line");
-    } else {
-        Serial.println("Both off line");
-    }
-
-    delay(200);
+  delay(100);
 }
+
